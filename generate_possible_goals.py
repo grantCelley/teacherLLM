98173dc1d37724b0e076c74c4ea87bde05fe7d79
Grant Celley <grant.celley@gmail.com>
2024-05-18 22:05:24 -0400

    Generated the goals purly from the LLM. Also identified possible reasons the goals aren't good.

--- generate_possible_goals.py
+++ generate_possible_goals.py
@@ -1,6 +1,6 @@
 import guidance
 from guidance import gen, select
-
+from tqdm import tqdm
 import os
 
 import json
@@ -12,186 +12,83 @@ model_kwargs = {"verbose": False, "n_gpu_layers": 256, "n_ctx": 30000}
 
 model = guidance.models.LlamaCppChat(MODEL, echo=False, **model_kwargs)
 
-
-
-def generate_topics(course_name: str, summary: str):
+def generate_chapters(title: str, level: str = "undergraduate") -> list[str]:
     """
-    This will generate topics for a course
-    :param course_name: Name of the course to generate the topics for
-    :param summary: Summary from wikipedia about the overall topic
-    :returns: List[str] List of the topics
+    Generates chapter topics for the course
+     :param title: Title of the course
+     :param level: A string for the the level education of the course. eg. "elementry" , "highschool", "undergraduate", "graduate" 
+     :return: The list of generated chapter titles
     """
 
-    schema = {
-        "type": "object",
-        "properties": {
-            "topics": {
-            "type": "array",
-            "items": {
-                "type": "string",
-                "description": "A topic"
-            }
-            }
-        },
-        "required": [
-            "topics"
-        ]
-        }
-
-    system_prompt = f"You are a professor creating a course for {course_name}. I will give you a wikipedia summary. Generate the topics for the course. Output JSON in the following schema {schema}"
-    user_prompt = summary
-
-    topics = None
-    while topics == None:
-        with guidance.system():
-            lm = model + system_prompt
-        with guidance.user():
-            lm = lm + user_prompt
-        with guidance.assistant():
-            lm = lm + f"""{{
-            "topics":[ """
-            for i in range(10):
-                lm += "\"" + gen("topics", list_append=True, max_tokens=10, stop='"') +  "\""
-                if i < 9:
-                    lm += ",\n"
-
-        topics = lm['topics']
-
-    return topics
-
-def is_section_Important(course_name: str, section:str, topic_list: list[str]):
-    """ 
-    This will check if the course is important based on the given course name and the given topic list
-    :param course_name: Name of the course to generate the topics for
-    :param summary: Summary from wikipedia about the overall topic
-    :returns: List[str] List of the topics
-    """
-
-    topic_list.append("None")
-    topic_str = '\n* '.join(topic_list)
-    topic_str = '\n* ' + topic_str
-
-    system_prompt = f"You are a professor creating a course for {course_name}. I will give you a wikipedia section and a list of topics. Tell me if the section would be part of any of the topics. If the section does not fall into any of the topics just type 'None'. The topics are:\n{topic_str}"
-
-    user_prompt = f"Wikipedia Section:\n{section}\n\n Topics:{topic_str}\nWhat topic is would the selection be part of? Just classify it."
-
-    topic = None
-    with guidance.system():
-        lm = model + system_prompt
     with guidance.user():
-        lm += user_prompt
+        lm = model + f"You are a teacher teaching about titled {title} at the level of {level}. Generate a 14 chapter course on the topic."
     with guidance.assistant():
-        lm += select(topic_list, 'topic')
-    
-    topic = lm['topic']
-    print(topic)
-    if topic == "None":
-        return False, 
-    else:
-        return True
-    
-    
-def generate_Objectives(title, section):
-    """ 
-    This will generate learning objectives
-    :param: title: The title of the course
-    :param: section: A section of wikipedia
-    :return: A list of learning objectives for a course
-    """ 
+        for i in range(14):
+            lm += "Chapter" + str(i) +" : " +  gen(name='chapters', list_append=True, max_tokens= 50, stop=['\n', 'Chapter', '.'] ) + "\n"
 
-    schema = {
-        "description": "The goals object",
-        "type": "object",
-        "properties": {
-            "goals": {
-            "type": "array",
-            "items": {
-                "type": "string",
-                "description": "A learning objective"
-            }
-            }
-        },
-        "required": [
-            "goals"
-        ]
-    }
+    return lm['chapters']
+
+def gen_goals(course_title:str, chapter_title: str, level: str ="undergraduate") -> list[str]:
+    """
+    Generate the course objectives
+     :param course_title: Title of the course
+     :param chapter_title: Chapter title
+     :param level: A string for the the level education of the course. eg. "elementry" , "highschool", "undergraduate", "graduate". Default is "undergraduate"
+    :return: The list of generated course objectives
+    """
 
     action_verbs_list = ["implement", 'design' , 'construct', 'develop', 'produce', 'revise', 'propose', 'build', 'devise', 'invent', 'judge', 'justify', 'select', 'critique', 'defend', 'rate', 'evaluate', 'assess', 'rank', 'argue', 'review', 'distinguish', 'differentiate', 'orginize', 'examine', 'compare', 'contrast', 'classify', 'apply', 'use', 'solve', 'compute', 'implement', 'instruct', 'demonstrate', 'interpret', 'complete', 'explain', 'demonstrate', 'summarize', 'review', 'generalize', 'describe', 'identify', 'represent', 'paraphrase', 'interpret', 'define', 'identify', 'recall', 'recite', 'reproduce', 'list', 'name', 'memorize', 'repeat', 'state', 'duplicate', 'match']
 
-    system = f"You are a teacher creating the course objectives for {title}. I will give you a wikipedia section and you will generate course objectives. The output is in JSON. The schema is {schema}"
-    
-    numberOfGoals = 5
-
     with guidance.system():
-        lm = model + system
+        lm = model + f"You are a teacher teaching about the course {course_title} at the level of {level}. Generate the course objectives. I will give you he topic"
     with guidance.user():
-        lm += section
+        lm += "The course objectives"
     with guidance.assistant():
-        lm + """{
-        goals: ["""
-
-        for i in range(numberOfGoals):
-            lm += '"The student will be able to' + select(action_verbs_list, "goal_actions", list_append=True) + gen('end_goals', list_append=True, max_tokens=50, stop=['"', '.',]) + '"'
-            if i < numberOfGoals - 1:
-                lm += ",\n"
-            
+        lm += chapter_title
+    with guidance.assistant():
+        lm += "The course objectives are: \n"
+        cont = True
+        for i in range(10):
+            if cont:
+                lm += "The student will be able to" + select(action_verbs_list, name='verbs', list_append=True) + gen(name='goal_end', list_append=True, max_tokens= 50, stop=['\n'] )
+                action_verbs_list.remove(lm['verbs'][i])
+        
         goals = []
-        for i in range(numberOfGoals):
-            goal = "The student will be able to " + lm['goal_actions'][i] + "" + lm['end_goals'][i]
+        for key, goal_end in enumerate(lm['goal_end']):
+            goal = "The student will be able to " + lm['verbs'][key] + goal_end
             goals.append(goal)
-
-    return goals
-
-   
-
-pages_file_names = glob.glob("./page_content/*.json")
-
-
-print(pages_file_names)
-
-for file_name in pages_file_names:
-    page_content = {}
-    with open(file_name, 'r') as f:
-        page_content = json.loads(f.read())
+        
+        return goals
     
-    title = page_content["title"]
-    key = page_content['key']
-    goal_sections = []
-    sections = page_content['sections']
+courses = glob.glob("page_content/*.json")
 
-    page_topics = generate_topics(course_name=title, summary=sections[0]['content'])
-    print(sections[0]['content'])
-    print("Topics generated: ")
-    print(page_topics)
-
-    relevant_sections = []
-    for section in sections[1:]:
-        content = section['content']
-        print(section['title'])
-        name = section['title']
-        if name != " References " and name != " See also " and name != " Sources " and name != " Citations " and name != " Notes " and name != " External links ": 
-            relevant = is_section_Important(title, content, page_topics)
-            print(relevant)
-            if relevant == True:
-                relevant_sections.append(section)
-    final_sections = []
-    for section in relevant_sections:
-        content = section['content']
-        section_title = section['title']
-        section['goals'] = generate_Objectives(title, content)
-        print(section_title)
-        print(section['goals'])
-        final_sections.append(section)
+for course in tqdm(courses):
+    content = { }
+    with open(course, "r", encoding="utf-8") as f:
+        content = json.load(f)
     
-    new_page_content ={
+    title = content['title']
+    key = content['key']
+    sections = content["sections"]
+
+    chapters_titles = generate_chapters(title)
+
+    chapters =  []
+    for chapter_title in chapters_titles:
+        chapter = {
+            "title": chapter_title,
+            "goals": gen_goals(title, chapter_title) 
+        }
+        chapters.append(chapter)
+    
+    new_page = {
         "title": title,
-        "key": key,
-        "summary": sections[0]['content'],
-        "sections": final_sections
+        "chapters": chapters
     }
 
-    file_path = "goal_pages/v1/" + title + ".json"
-    if not os.path.exists(file_path):
-        os.mknod(file_path)
-    with open(file_path,"w") as f:
-        json.dump(new_page_content,f)
+    path = "goal_pages/v1.1/"+ title + ".json"
+    if os.path.exists(path):
+        os.mknod(path)
+    with open(path, 'w+') as f:
+        json.dump(new_page,f)
+
---
+++ learning goals goals.md
@@ -0,0 +1,15 @@
+These are the tests for the learning goals. We want to rewrite them and remove duplicates
+
+What could possibly go wrong?
+
+Well here is a list:
+* The goal is not mesurable.
+* The goal is continues over a sentance
+* The goal has multiple action verbs
+* The goal is a duplicate
+* The goal is too broad
+* The goal is not gramatically correct
+* The goal should be broken down into smaller parts
+* The goal is too unrealistic with time
+* The goal does not makes sense in the context of the course
+* the goal is too specific on how it is implemented
